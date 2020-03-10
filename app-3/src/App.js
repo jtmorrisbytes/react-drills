f8fb45b8ab6a40f258538e3435c6216246cf72d3
= <jthecybertinkerer@gmail.com>
2020-03-10 16:57:03 -0500

    finished app 3

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      foodList: ["apples", "oranges", "sausages", "hamburgers", "cheese"],
+      userInput: ""
+    };
+  }
+  handleInputChange = event => {
+    this.setState({ userInput: event.target.value });
+  };
+  render() {
+    let foodList = this.state.foodList
+      .filter(food => {
+        if (this.state.userInput.length > 0) {
+          return food.includes(this.state.userInput);
+        } else return true;
+      })
+      .map((food, foodIndex) => {
+        return <li key={"foodItem-" + foodIndex}>{food}</li>;
+      });
+
+    return (
+      <div id="App">
+        <h1>The Filtered Food List</h1>
+        <input
+          type="text"
+          value={this.state.userInput}
+          onChange={this.handleInputChange}
+        />
+        <ul id="food-list">{foodList}</ul>
+      </div>
+    );
+  }
 }
 
 export default App;
