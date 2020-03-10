c3e3d89c7eacc12b8a431e5a27b16c21f93b4350
= <jthecybertinkerer@gmail.com>
2020-03-10 16:33:18 -0500

    finished App 2

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      foodList: ["apples", "oranges", "sausages", "hamburgers", "cheese"]
+    };
+  }
+  render() {
+    let foodList = this.state.foodList.map((food, foodIndex) => {
+      return <li key={"foodItem-" + foodIndex}>{food}</li>;
+    });
+
+    return (
+      <div id="App">
+        <h1>The food List</h1>
+        <ul id="food-list">{foodList}</ul>
+      </div>
+    );
+  }
 }
 
 export default App;
