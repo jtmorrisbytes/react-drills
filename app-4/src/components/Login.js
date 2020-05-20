633947e91aae1296bf1bbea3329fb17dfc717eb2
= <=>
2020-05-19 21:55:13 -0500

    assignment 4 is ready to turn in

--- app-4/src/components/Login.js
+++ app-4/src/components/Login.js
@@ -1,37 +1,41 @@
 import React from "react";
-
+import "./Login.css";
 export default class Login extends React.Component {
   constructor() {
     super();
     this.state = {
       username: "",
-      password: ""
+      password: "",
     };
+    this.handleLogin = this.handleLogin.bind(this);
   }
-  handleInputChange = event => {
+  handleInputChange = (event) => {
     this.setState({ [event.target.id]: event.target.value });
   };
-  handleLogin = () => {
+  handleLogin(e) {
+    e.preventDefault();
     alert(`Username:${this.state.username}, Password ${this.state.password}`);
     this.setState({ username: "", password: "" });
-  };
+  }
   render() {
     return (
-      <div id="Login">
+      <form id="Login" onSubmit={this.handleLogin}>
         <input
           id="username"
           onChange={this.handleInputChange}
           value={this.state.username}
         />
+        <br />
         <input
           id="password"
           value={this.state.password}
           onChange={this.handleInputChange}
         />
+        <br />
         <button type="button" onClick={this.handleLogin}>
           Login
         </button>
-      </div>
+      </form>
     );
   }
 }
