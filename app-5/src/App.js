7a29eb622c78cccecfcbc28a3dfacc4920a614ae
Jordan Morris <jthecybertinkerer@gmail.com>
2020-06-01 15:07:53 -0500

    finished app-5

--- app-5/src/App.js
+++ app-5/src/App.js
@@ -2,23 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Image from "./Image"
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Image src="https://i.picsum.photos/id/1001/800/600.jpg" />
     </div>
   );
 }
